bb69c1462572eda5c8597ef5e9f0469843b3104c
Mark Scheer <mscheer314@gmail.com>
2021-03-25 19:46:15 -0500

    Finish parts 2 and 3 of assignment

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -9,7 +9,7 @@ export class FavPhotosComponent implements OnInit {
   photosTitle = 'Sweet Photos of Dietrich and Nacho!';
   image1 = '../assets/IMG_0823.jpg';
   image2 = '../assets/IMG_1173.jpg';
-  image3 = '';
+  image3 = '../assets/IMG_0865.jpg';
 
   constructor() { }
 
