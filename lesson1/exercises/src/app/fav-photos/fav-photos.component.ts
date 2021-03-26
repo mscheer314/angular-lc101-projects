80e9149460c05856b428309e8177c032ce9857dd
Mark Scheer <mscheer314@gmail.com>
2021-03-25 19:05:22 -0500

    Add photos

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,9 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
+  photosTitle = 'Sweet Photos of Dietrich and Nacho!';
+  image1 = '../assets/IMG_0823.jpg';
+  image2 = '../assets/IMG_1173.jpg';
   image3 = '';
 
   constructor() { }
